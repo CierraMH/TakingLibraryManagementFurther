21e13c10af2b5f50df32d34f7331cc9c9570742b
CierraMH <ceceminorhampton@gmail.com>
2023-05-22 22:14:43 -0400

    formatted

--- src/Assignment/Book.java
+++ src/Assignment/Book.java
@@ -17,6 +17,7 @@ public class Book {
         this.publicationYear = publicationYear;
         this.pages = pages;
     }
+
     public Book(String title, String author, int publicationYear, int pages, String category) {
         this.title = title;
         this.author = author;
@@ -25,12 +26,12 @@ public class Book {
         this.category = category;
         this.isOnLoan = false;
 
-        
     }
- 
+
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -38,6 +39,7 @@ public class Book {
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author) {
         this.author = author;
     }
@@ -45,6 +47,7 @@ public class Book {
     public int getPublicationYear() {
         return publicationYear;
     }
+
     public void setPublicationYear(int publicationYear) {
         this.publicationYear = publicationYear;
     }
@@ -52,6 +55,7 @@ public class Book {
     public int getPages() {
         return pages;
     }
+
     public void setPages(int pages) {
         this.pages = pages;
     }
@@ -59,6 +63,7 @@ public class Book {
     public String getCategory() {
         return category;
     }
+
     public void setCategory(String category) {
         this.category = category;
     }
@@ -66,6 +71,7 @@ public class Book {
     public boolean isOnLoan() {
         return isOnLoan;
     }
+
     public void setIsOnLoan(boolean isOnLoan) {
         this.isOnLoan = isOnLoan;
     }
@@ -73,12 +79,16 @@ public class Book {
     public LocalDate getDueDate() {
         return dueDate;
     }
+
     public void setDueDate(LocalDate dueDate) {
         this.dueDate = dueDate;
     }
+
     @Override
-    public String toString(){
-        return "Book Information: Title " + title + ", Author " + author + ", Publication year " + publicationYear + ", Number of pages " + pages + ". Is the Book in the Fiction or Non-Fiction category: " + category + ".\n";
+    public String toString() {
+        return "Book Information: Title " + title + ", Author " + author + ", Publication year " + publicationYear
+                + ", Number of pages " + pages + ". Is the Book in the Fiction or Non-Fiction category: " + category
+                + ".\n";
     }
 
 }
--- src/Assignment/User.java
+++ src/Assignment/User.java
@@ -7,25 +7,30 @@ public class User {
     private String name;
     private String libraryCardNumber;
     List<Book> bookOnLoan;
-    
+
     public User(String name, String libraryCardNumber) {
         this.name = name;
         this.libraryCardNumber = libraryCardNumber;
         this.bookOnLoan = new ArrayList<>();
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
-    public String libraryCardNumber(){
+
+    public String libraryCardNumber() {
         return libraryCardNumber;
     }
-    public void addBook(Book book){
+
+    public void addBook(Book book) {
         bookOnLoan.add(book);
     }
-    public void removeBook(Book book){
+
+    public void removeBook(Book book) {
         bookOnLoan.remove(book);
     }
-    public boolean hasBook(Book book){
+
+    public boolean hasBook(Book book) {
         return bookOnLoan.contains(book);
     }
 }
